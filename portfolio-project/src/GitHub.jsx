a6eb65ab1509a97ac97e8d21d2d9ca3b2affbb9b
Remy-Post <166053369+Remy-Post@users.noreply.github.com>
2025-12-18 18:49:41 -0500

    Enhanced GitHub component by adding a title section and improving layout structure.

--- portfolio-project/src/GitHub.jsx
+++ portfolio-project/src/GitHub.jsx
@@ -1,7 +1,7 @@
 export default function GitHub() {
   return (
       <>
-          <div>
+          <div className="">
             <h1 className="text-3xl">GitHub</h1>
           </div>
           <a className="hover:underline text-blue-500" href="https://github.com/Remy-Post" target="_blank">GitHub</a>
