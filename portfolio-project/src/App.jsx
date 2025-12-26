21b2eed915e67a1045d62eccadd0ac343fe2fc2a
Remy-Post <166053369+Remy-Post@users.noreply.github.com>
2025-12-26 08:57:41 -0500

    UI improvements

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -5,10 +5,9 @@ function App() {
   return (
     <>
         <div className="w-[90%] mx-auto border-amber-50 bg-white">
-            <header className="flex justify-between h-[10%] py-4 grid-rows-1 bg-gray-50 items-center">
-                <h1 className="text-3xl">Portfolio</h1>
-                <nav className="-bg-linear-150 ml-10">
-                     <menu className=" w-[50vw] text-xl flex justify-between mx-1">
+            <header className="flex justify-between h-[10%] py-4 bg-gray-50 items-center">
+                <nav className="mx-auto">
+                     <menu className="flex justify-between gap-5">
                          <NavItem to="/" >Home</NavItem>
                          <NavItem to="/projects">Projects</NavItem>
                          <NavItem to="/contacts">Contact</NavItem>
@@ -32,7 +31,7 @@ export default App
 function NavItem ({children, to}){
     return (
          <li className="cursor-pointer">
-             {to ? <Link to={to} className="p-2 hover:bg-gray-500 hover:underline rounded-lg">{children}</Link> : children}
+             {to ? <Link to={to} className="p-2 hover:bg-gray-500 hover:underline rounded-lg underline underline-offset-2">{children}</Link> : children}
          </li>
     )
 }
--- portfolio-project/src/Index.jsx
+++ portfolio-project/src/Index.jsx
@@ -2,7 +2,12 @@ export default function Index(){
     return(
         <>
           {/* Header */}
-          <h1 className="w-full h-[30vh] bg-amber-50 flex justify-center items-center">Remy Post</h1>
+          <div className="w-full h-[30vh] bg-amber-50 grid grid-cols-[40%_60%]">
+            <div className="flex flex-col justify-center">
+                <h1 className="">Hi, I'm Remy Post</h1>
+                <h3 className="">A Computer Science Student passionate in Web Development</h3>
+            </div>
+          </div>
 
            <div className="w-full h-[50vh] bg-amber-200 grid grid-rows-[30%_70%]">
                <h3 className="text-2xl text-center self-center">
